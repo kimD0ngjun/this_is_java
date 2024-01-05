cb8ee2cccb4d9ea5dfdbe34060cc05284bc2eab1
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-05 17:46:52 +0900

    [1.5] 17강, 배열로부터 스트림

---
+++ src/section_17_stream/ex04_resource/arrays/StreamExample.java
@@ -0,0 +1,20 @@
+package section_17_stream.ex04_resource.arrays;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class StreamExample {
+    public static void main(String[] args) {
+        String[] strings = {"홍길동", "김자바", "치킨냠"};
+        Stream<String> stringStream = Arrays.stream(strings); // 데이터 처리를 위한 스트림화
+        stringStream.forEach(item -> System.out.print(item + ", "));
+
+        System.out.println();
+
+        int[] ints = {1, 2, 3, 4, 5};
+//        Stream<Integer> integerStream = Arrays.stream(ints); // 에러가 난 이유는 그냥 Stream은 문자열 요소 스트림화
+        IntStream intStream = Arrays.stream(ints); // int 배열의 데이터 처리를 위한 intStream화
+        intStream.forEach(item -> System.out.print(item + ", "));
+    }
+}
