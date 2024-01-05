d33677cdfeae92419269896e2c31ab870a100d63
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-05 18:38:54 +0900

    [1.5] 17강, 파일로부터 스트림

---
+++ src/section_17_stream/ex04_resource/file/StreamExample.java
@@ -0,0 +1,26 @@
+package section_17_stream.ex04_resource.file;
+
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class StreamExample {
+    public static void main(String[] args) throws Exception {
+        Path path = Paths.get(StreamExample.class.getResource("data.txt").toURI());
+        // Path의 경로를 URI 객체로 변환 후 반환
+
+//        Stream<String> stream = Files.lines(path, Charset.defaultCharset());
+//        // lines() 파일에서 모든 line을 읽어서 Stream으로 리턴
+//
+//        stream.forEach(line -> System.out.println(line));
+//        stream.close(); // 자원 누수를 방지하기 위한 close() 메소드
+
+        try (Stream<String> stream = Files.lines(path, Charset.defaultCharset())) {
+            stream.forEach(line -> System.out.println(line));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } // 리소스 닫기를 통한 try-with-resources 문법(얘도 자원 누수 방지)
+    }
+}
