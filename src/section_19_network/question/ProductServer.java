d08b69b9de4b941e8bdfd4d0dfc619a3ab0650d0
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-11 08:44:56 +0900

    [1.11] 19강, SocketClient 필수 필드 선언

--- src/section_19_network/question/ProductServer.java
+++ src/section_19_network/question/ProductServer.java
@@ -1,19 +1,34 @@
 package section_19_network.question;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ProductServer {
     ServerSocket serverSocket;
     ExecutorService threadPool = Executors.newFixedThreadPool(100);
-    Map<String, SocketClient> shopRoom = Collections.synchronizedMap(new HashMap<>());
+    List<SocketClient> products = Collections.synchronizedList(new ArrayList<>());
 
     class SocketClient {
+        ProductServer productServer;
+        Socket socket;
+        DataInputStream dis;
+        DataOutputStream dos;
 
+        public SocketClient(ProductServer productServer, Socket socket) throws IOException {
+            this.productServer = productServer;
+            this.socket = socket;
+            this.dis = new DataInputStream(socket.getInputStream());
+            this.dos = new DataOutputStream(socket.getOutputStream());
+        }
     }
 }
