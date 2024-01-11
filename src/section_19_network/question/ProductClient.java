e590d8e653fa6ae5d8c6a86886ec571bab2b1c99
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-11 09:59:30 +0900

    [1.11] 클라이언트 JSON 수신 메소드 작성

--- src/section_19_network/question/ProductClient.java
+++ src/section_19_network/question/ProductClient.java
@@ -4,6 +4,8 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class ProductClient {
     // 필드
@@ -17,5 +19,27 @@ public class ProductClient {
         dis = new DataInputStream(socket.getInputStream());
         dos = new DataOutputStream(socket.getOutputStream());
         System.out.println("[client] connected");
+    }
+
+    public void receive() {
+        Thread thread = new Thread(() -> {
+           try {
+               while(true) {
+                   String json = dis.readUTF();
+                   JSONParser parser = new JSONParser();
+                   JSONObject root = (JSONObject)  parser.parse(json);
+
+                   int no = (int) root.get("no");
+                   String name = (String) root.get("name");
+                   int price = (int) root.get("int");
+                   int stock = (int) root.get("stock");
+                   System.out.println(no + " " + name + " " + price + " " + stock);
+               }
+           } catch(Exception e) {
+               System.out.println("[client] disconnected");
+               System.exit(0);
+           }
+        });
+        thread.start();
     }
 }
