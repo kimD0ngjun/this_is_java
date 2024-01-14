3303d1de5e158c44f4bf04aedb476e488e885e95
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-14 19:11:02 +0900

    [1.14] 20강, 데이터 읽기용 Board 클래스

---
+++ src/section_20_database/ex09_read_data/Board.java
@@ -0,0 +1,72 @@
+package section_20_database.ex09_read_data;
+
+import java.util.Date;
+
+public class Board {
+    private int bno;
+    private String btitle;
+    private String bcontent;
+    private String bwriter;
+    private Date bdate;
+    private String bfilename;
+
+    public int getBno() {
+        return bno;
+    }
+
+    public void setBno(int bno) {
+        this.bno = bno;
+    }
+
+    public String getBtitle() {
+        return btitle;
+    }
+
+    public void setBtitle(String btitle) {
+        this.btitle = btitle;
+    }
+
+    public String getBcontent() {
+        return bcontent;
+    }
+
+    public void setBcontent(String bcontent) {
+        this.bcontent = bcontent;
+    }
+
+    public String getBwriter() {
+        return bwriter;
+    }
+
+    public void setBwriter(String bwriter) {
+        this.bwriter = bwriter;
+    }
+
+    public Date getBdate() {
+        return bdate;
+    }
+
+    public void setBdate(Date bdate) {
+        this.bdate = bdate;
+    }
+
+    public String getBfilename() {
+        return bfilename;
+    }
+
+    public void setBfilename(String bfilename) {
+        this.bfilename = bfilename;
+    }
+
+    @Override
+    public String toString() {
+        return "Board{" +
+                "bno=" + bno +
+                ", btitle='" + btitle + '\'' +
+                ", bcontent='" + bcontent + '\'' +
+                ", bwriter='" + bwriter + '\'' +
+                ", bdate=" + bdate +
+                ", bfilename='" + bfilename + '\'' +
+                '}';
+    }
+}
