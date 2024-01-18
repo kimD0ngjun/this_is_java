d5e13bd9bce17ac099b959cab483d3177587cf5a
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-18 13:30:36 +0900

    [1.18] 7강, 오버라이딩 메소드 실행

---
+++ src/section_07/ex07/Example.java
@@ -0,0 +1,9 @@
+package section_07.ex07;
+
+public class Example {
+    public static void main(String[] args) {
+        Parent e = new Child();
+        e.method1();
+        e.method2();
+    }
+}
