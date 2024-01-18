4397ded4678c31eb00ae2e7d9e9c9799573182cc
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-18 13:29:10 +0900

    [1.18] 7강, 다형성 구현 전제 조건인 타입 변환용 자식 클래스

---
+++ src/section_07/ex07/Child.java
@@ -0,0 +1,14 @@
+package section_07.ex07;
+
+public class Child extends Parent {
+    public String field2;
+
+    @Override
+    public void method2() {
+        System.out.println("오버라이딩 된 부모 메소드 2");
+    }
+
+    public void method3() {
+        System.out.println("자식 메소드 3");
+    }
+}
