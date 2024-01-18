c85a825880b24499d639a2f81a58d7e1797d07eb
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-18 13:28:04 +0900

    [1.18] 7강, 다형성 구현 전제 조건인 타입 변환용 부모 클래스

---
+++ src/section_07/ex07/Parent.java
@@ -0,0 +1,13 @@
+package section_07.ex07;
+
+public class Parent {
+    public String field1;
+
+    public void method1() {
+        System.out.println("부모 메소드 1");
+    }
+
+    public void method2() {
+        System.out.println("부모 메소드 2");
+    }
+}
