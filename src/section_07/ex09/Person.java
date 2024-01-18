af16a7d038f0be8bdc3706c04d859eadbbc648c0
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-18 13:40:44 +0900

    [1.18] 7강, instanceof 연산자 확인용 부모 클래스

---
+++ src/section_07/ex09/Person.java
@@ -0,0 +1,13 @@
+package section_07.ex09;
+
+public class Person {
+    String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public void walk() {
+        System.out.println("걷기");
+    }
+}
