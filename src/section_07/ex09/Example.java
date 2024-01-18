35419e8b1ce20d3d578d62d0fe0c4c14827a840c
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-18 13:57:00 +0900

    [1.18] 7강, 메인 메소드 작성

--- src/section_07/ex09/Example.java
+++ src/section_07/ex09/Example.java
@@ -2,7 +2,12 @@ package section_07.ex09;
 
 public class Example {
     public static void main(String[] args) {
+        Person person = new Person("무명");
+        Person student = new Student("유명", 17);
 
+        personInfo(person);
+        System.out.println();
+        personInfo(student);
     }
 
     public static void personInfo(Person person) {
