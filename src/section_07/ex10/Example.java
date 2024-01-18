0c1f1945fe104c9a973c9cce6ba2da97ff8dcd72
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-18 14:27:59 +0900

    [1.18] 7강, 추상 클래스 예시

---
+++ src/section_07/ex10/Example.java
@@ -0,0 +1,16 @@
+package section_07.ex10;
+
+public class Example {
+    public static void main(String[] args) {
+//        Animal animal = new Animal("동물"); // 불가능
+        Animal animal = new Dog("진돌이");
+        System.out.println("이름 : " + animal.name);
+        animal.breathe();
+        animal.sound();
+
+        animal = new Cat("애용이");
+        System.out.println("이름 : " + animal.name);
+        animal.breathe();
+        animal.sound();
+    }
+}
