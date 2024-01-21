acc8aad7fbbc4f52ef3cb2fa69986079e629cb11
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-21 17:51:24 +0900

    [1.21] 9강, 익명 구현 객체 완료 - 다형성

--- src/section_10_nested_class/ex07/Example.java
+++ src/section_10_nested_class/ex07/Example.java
@@ -1,6 +1,7 @@
 package section_10_nested_class.ex07;
 
 import section_10_nested_class.ex07.anonymous_child.ExternalClass;
+import section_10_nested_class.ex07.anonymous_implements.ExternalClass2;
 
 public class Example {
     public static void main(String[] args) {
@@ -9,6 +10,7 @@ public class Example {
 
         System.out.println();
 
-
+        ExternalClass2 externalClass2 = new ExternalClass2();
+        externalClass2.externalMethod2();
     }
 }
