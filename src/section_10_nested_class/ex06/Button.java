da54f0dfe4a45aa4c47d548eb144828839d13b34
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-21 16:51:03 +0900

    [1.21] 9강, 중첨 정적 인터페이스 작성

---
+++ src/section_10_nested_class/ex06/Button.java
@@ -0,0 +1,9 @@
+package section_10_nested_class.ex06;
+
+public class Button {
+    // static nested interface
+    public static interface ClickListener {
+        // abstract method
+        void onClick();
+    }
+}
