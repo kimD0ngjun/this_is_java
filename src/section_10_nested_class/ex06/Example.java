639cc76559c4c019ada8fbca5368dc745d8f3382
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-21 16:44:33 +0900

    [1.21] 9강, 접근 테스트

---
+++ src/section_10_nested_class/ex06/Example.java
@@ -0,0 +1,10 @@
+package section_10_nested_class.ex06;
+
+public class Example {
+    public static void main(String[] args) {
+        A a = new A();
+        a.useBInnerPrint();
+        System.out.println();
+        a.userBOuterPrint();
+    }
+}
