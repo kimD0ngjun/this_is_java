b7374ff7151bcef61e71bf39f9ecb5d89bffe90c
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-22 23:51:32 +0900

    [1.22] 11강, 리소스 자동 폐쇄 예제 테스트

--- src/section_11_exception/ex04/AutoCloseableExample.java
+++ src/section_11_exception/ex04/AutoCloseableExample.java
@@ -2,9 +2,36 @@ package section_11_exception.ex04;
 
 public class AutoCloseableExample {
     public static void main(String[] args) {
-        // 아까 FileInputStream은 AutoCloseable 인터페이스를 구현하고 있음
+        // 아까 FileInputStream도 AutoCloseable 인터페이스를 구현하고 있음(close() 메소드 재정의)
+        // 여기서 쓰는 Resource 클래스도 AutoCloseable 인터페이스를 구현해서 close() 메소드 재정의
         // try-with-resource 블록을 사용하면 예외 발생 여부와 상관없이 리소스 자동 폐쇄
 
+        try (Resource res = new Resource("A")) {
+            String data = res.read1();
+            int value = Integer.parseInt(data);
+        } catch(Exception e) {
+            System.out.println("예외 처리: " + e.getMessage());
+        }
 
+        System.out.println();
+
+        try (Resource res = new Resource("A")) {
+            String data = res.read2();
+            int value = Integer.parseInt(data); // NFE 발생
+        } catch(Exception e) {
+            System.out.println("예외 처리: " + e.getMessage());
+        }
+
+        System.out.println();
+
+        Resource res1 = new Resource("A");
+        Resource res2 = new Resource("B");
+
+        try (res1; res2) {
+            String data1 = res1.read1();
+            String data2 = res2.read1();
+        } catch(Exception e) {
+            System.out.println("예외 처리: " + e.getMessage());
+        }
     }
 }
