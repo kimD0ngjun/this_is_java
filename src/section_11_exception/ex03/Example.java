71a2bc223f76362358dd07a48aa4d280e75ad3c8
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-22 23:01:31 +0900

    [1.22] 11강, 예외 핸들링을 위한 메소드 작성

---
+++ src/section_11_exception/ex03/Example.java
@@ -0,0 +1,11 @@
+package section_11_exception.ex03;
+
+public class Example {
+    public static void main(String[] args) {
+        String[] array = {"100", "1oo"};
+
+        for(int i=0; i< array.length; i++) {
+
+        }
+    }
+}
