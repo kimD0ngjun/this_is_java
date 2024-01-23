20e4af217a631f1f3aacd78c9f0627fd64a9cee9
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-23 15:46:57 +0900

    [1.24] 12강, Object 클래스 상속 테스트를 위한 기본 클래스 새엇ㅇ

---
+++ src/section_12_base_module/ex03/Example.java
@@ -0,0 +1,13 @@
+package section_12_base_module.ex03;
+
+public class Example {
+    private String id;
+    private int number;
+    private String info;
+
+    public Example(String id, int number, String info) {
+        this.id = id;
+        this.number = number;
+        this.info = info;
+    }
+}
