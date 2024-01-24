3b2f972206b5e904ea835e78e49ad5d5cefdb5aa
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-24 15:15:38 +0900

    [1.24] 12강, 멤버 정보 얻기

--- src/section_12_base_module/ex11/Car.java
+++ src/section_12_base_module/ex11/Car.java
@@ -1,4 +1,80 @@
 package section_12_base_module.ex11;
 
-public record Car(String carName, int price, String driver, int velocity) {
+import java.util.Objects;
+
+public class Car {
+    private String carName;
+    private int price;
+    private String driver;
+    private int velocity;
+
+    // 필드를 초기화하는 생성자
+    public Car(String carName, int price, String driver, int velocity) {
+        this.carName = carName;
+        this.price = price;
+        this.driver = driver;
+        this.velocity = velocity;
+    }
+
+    // 매개변수 없는 생성자
+    public Car() {
+        // 기본적으로 아무 동작도 하지 않음
+    }
+
+    // Getter 메소드들
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public int getVelocity() {
+        return velocity;
+    }
+
+    // Setter 메소드들
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public void setVelocity(int velocity) {
+        this.velocity = velocity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return price == car.price && velocity == car.velocity && Objects.equals(carName, car.carName) && Objects.equals(driver, car.driver);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(carName, price, driver, velocity);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "carName='" + carName + '\'' +
+                ", price=" + price +
+                ", driver='" + driver + '\'' +
+                ", velocity=" + velocity +
+                '}';
+    }
 }
--- src/section_12_base_module/ex11/GetClass.java
+++ src/section_12_base_module/ex11/GetClass.java
@@ -1,15 +1,63 @@
 package section_12_base_module.ex11;
 
+// 자바는 클래스와 인터페이스의 메타 정보를 Class 객체로 관리한다.
+// 메타 정보 : 패키지 정보, 타입 정보, 멤버(생성자, 필드, 메소드) 정보 등을 일컫고
+// 이런 메타 정보를 프로그램에서 읽고 수정하는 행위를 리플렉션(reflection)이라고 한다.
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class GetClass {
     public static void main(String[] args) {
         Class classInfo = Car.class;
 
         getClassInfo(classInfo);
+        System.out.println();
+        getMemberInfo(classInfo);
     }
 
     public static void getClassInfo(Class classInfo) {
         System.out.println("패키지 : " + classInfo.getPackageName());
         System.out.println("클래스 간단 이름 : " + classInfo.getSimpleName());
         System.out.println("클래스 전체 이름 : " + classInfo.getName());
+    }
+
+    public static void getMemberInfo(Class classInfo) {
+        System.out.println("[생성자 정보]");
+
+        Constructor[] constructors = classInfo.getDeclaredConstructors();
+        for (Constructor constructor: constructors) {
+            System.out.print(constructor.getName() + "(");
+            Class[] parameters = constructor.getParameterTypes();
+            printParameters(parameters);
+            System.out.println(")");
+        }
+
+        System.out.println("\n\n[필드 정보]");
+
+        Field[] fields = classInfo.getDeclaredFields();
+        for(Field field: fields) {
+            System.out.print(field.getType().getName() + " " + field.getName() + ", ");
+        }
+
+        System.out.println("\n\n[메소드 정보]");
+
+        Method[] methods = classInfo.getDeclaredMethods();
+        for(Method method: methods) {
+            System.out.print(method.getName() + "(");
+            Class[] parameters = method.getParameterTypes();
+            printParameters(parameters);
+            System.out.println(")");
+        }
+    }
+
+    private static void printParameters(Class[] parameters) {
+        for(int i=0; i<parameters.length; i++) {
+            System.out.print(parameters[i]. getName());
+            if (i< parameters.length - 1) {
+                System.out.print(", ");
+            }
+        }
     }
 }
