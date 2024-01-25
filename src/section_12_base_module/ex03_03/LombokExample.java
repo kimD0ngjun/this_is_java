2d25499b27eecf64d8a59c60c886a00e2a24c484
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-25 14:11:03 +0900

    [1.25] 12강, 라이브러리 삭제 후, 기본 플러그인으로 적용하려고 노력하는 중...

--- src/section_12_base_module/ex03_03/LombokExample.java
+++ src/section_12_base_module/ex03_03/LombokExample.java
@@ -1,14 +1,11 @@
-//package section_12_base_module.ex03_03;
-//
-//import lombok.NonNull;
-//import lombok.RequiredArgsConstructor;
-//
-//@RequiredArgsConstructor
-//public class LombokExample {
-//    @NonNull private String name;
-//    private final int age;
-//    private String id;
-//}
+package section_12_base_module.ex03_03;
+
+
+public class LombokExample {
+    private String name;
+    private int age;
+    private String id;
+}
 
 // 롬복은 왜 계속 에러가 나지?
 // 그냥 레코드 쓸까..?
