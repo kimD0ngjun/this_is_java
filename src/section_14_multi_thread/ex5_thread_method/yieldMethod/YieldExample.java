010df97d8ad0af20ef6ff11bbfb91eda02d5f249
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-28 17:59:07 +0900

    [1.28] 14강, yield() 메소드 설명 추가

--- src/section_14_multi_thread/ex5_thread_method/yieldMethod/YieldExample.java
+++ src/section_14_multi_thread/ex5_thread_method/yieldMethod/YieldExample.java
@@ -14,4 +14,8 @@ public class YieldExample {
         try { Thread.sleep(10000); } catch (InterruptedException e) {} // 10초 간 메인 스레드 중단
         threadA.work = true; // 다시 A와 B 번갈아가면서 작업처리 주르륵
     }
+
+    // threadA.work 변수의 통제를 통한 전체적인 제어 흐름의 역할은 메인 스레드가 맡고 있음
+    // 이 메인 스레드를 5초 동안 일시정지 상태로 보냈다가 5초 후에 다시 돌리면서 변수를 false로 지정한 다음
+    // 다시 곧바로 10초 동안 일시정지 상태로 보내서 다시 돌리면서 변수를 true로 재지정
 }
