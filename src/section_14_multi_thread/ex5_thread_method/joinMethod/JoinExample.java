cb8f54b5c82254d29f6b03f551ba007ecaf74dfb
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-28 17:02:50 +0900

    [1.28] 14강, join() 메소드 설명 추가

--- src/section_14_multi_thread/ex5_thread_method/joinMethod/JoinExample.java
+++ src/section_14_multi_thread/ex5_thread_method/joinMethod/JoinExample.java
@@ -10,6 +10,7 @@ public class JoinExample {
             // sumThread 스레드가 호출돼서 종료될 때까지 메인 메소드 실행 일시 정지
         } catch (InterruptedException e) {
         }
+        // join()을 호출한 sumThread 스레드가 종료 상태가 되면 메인 메소드가 다시 대기 상태로 돌아감
         System.out.println("1~100 합 : " + sumThread.getSum());
     }
 }
