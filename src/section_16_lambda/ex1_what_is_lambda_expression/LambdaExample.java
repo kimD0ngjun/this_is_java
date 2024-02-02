6244d5aa8fa1807cc8e19aa73ba71844344c0f06
kimD0ngjun <chickentasty0112@gmail.com>
2024-02-02 18:34:36 +0900

    [2.2] 16강, 람다식 정의 함수형 프로그래밍과 연관지어서 생각하기

--- src/section_16_lambda/ex1_what_is_lambda_expression/LambdaExample.java
+++ src/section_16_lambda/ex1_what_is_lambda_expression/LambdaExample.java
@@ -14,9 +14,10 @@ public class LambdaExample {
         }); // 얘도 마찬가지
     }
 
+    // action 메소드 블록 공간이 데이터 처리부
     public static void action(Calculable calculable) {
-        int x = 10;
-        int y = 4;
-        calculable.calculate(x, y);
-    }
+        int x = 10; // 덩그러니 놓여진 데이터
+        int y = 4; // 덩그러니 놓여진 데이터
+        calculable.calculate(x, y); // 그리고 제공된 데이터 처리 목적 함수
+    } // Wow! 함수형 프로그래밍
 }
