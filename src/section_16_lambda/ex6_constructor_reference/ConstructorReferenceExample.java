cbafccd4b2cc5e9dc577a73457e662ac55e9955a
kimD0ngjun <chickentasty0112@gmail.com>
2024-02-03 01:00:37 +0900

    [2.3] 16강, 생성자 참조 재확인

--- src/section_16_lambda/ex6_constructor_reference/ConstructorReferenceExample.java
+++ src/section_16_lambda/ex6_constructor_reference/ConstructorReferenceExample.java
@@ -5,11 +5,13 @@ public class ConstructorReferenceExample {
         Person person = new Person();
 
         Member member1 = person.getMember1(Member::new);
+//        Member member1 = person.getMember1((x) -> new Member(x));
         System.out.println(member1); // 오버라이딩한 toString() 메소드가 자동으로 호출
 
         System.out.println();
 
         Member member2 = person.getMember2(Member::new);
+//        Member member2 = person.getMember2((x, y) -> new Member(x, y));
         System.out.println(member2);
     }
 }
