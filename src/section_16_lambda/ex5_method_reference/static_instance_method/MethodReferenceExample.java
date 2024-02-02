71e53af941053c9cf57f486d99b3ad35c81dae01
kimD0ngjun <chickentasty0112@gmail.com>
2024-02-02 23:40:53 +0900

    [2.2] 16강, 람다식 메소드 참조 인텔리제이 기능 확인

--- src/section_16_lambda/ex5_method_reference/static_instance_method/MethodReferenceExample.java
+++ src/section_16_lambda/ex5_method_reference/static_instance_method/MethodReferenceExample.java
@@ -6,11 +6,16 @@ public class MethodReferenceExample {
 
         // 컴퓨터 클래스에 선언된 두 메소드를 람다식에 써먹을 거임
         // 정적 메소드일 경우의 람다식
+        person.action((x, y) -> Computer.staticCalculate(x, y));
+
         person.action(Computer::staticCalculate);
         // 핵심은 이제까지 일일이 적던 람다식을 다른 곳에 이미 선언된 메소드 내용을 참조하는 식
 
         // 인스턴스 메소드일 경우의 람다식
         Computer computer = new Computer();
+
+        person.action((x, y) -> computer.instanceCalculate(x, y));
+
         person.action(computer::instanceCalculate);
         // 정적 메소드는 클래스명을 땡땡이 앞에 붙이지만, 인스턴스 메소드는 참조 변수를 땡땡이 앞에 붙임
     }
