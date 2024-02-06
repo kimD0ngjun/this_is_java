e4273644f7e27d297aa1e8a3f7ed89c9010d7199
kimD0ngjun <chickentasty0112@gmail.com>
2024-02-06 22:52:03 +0900

    [2.6] stream.close()를 통한 자원 누수 방지

--- src/section_17_stream/ex04_resource/file/StreamExample.java
+++ src/section_17_stream/ex04_resource/file/StreamExample.java
@@ -11,16 +11,16 @@ public class StreamExample {
         Path path = Paths.get(StreamExample.class.getResource("data.txt").toURI());
         // Path의 경로를 URI 객체로 변환 후 반환
 
-//        Stream<String> stream = Files.lines(path, Charset.defaultCharset());
-//        // lines() 파일에서 모든 line을 읽어서 Stream으로 리턴
-//
-//        stream.forEach(line -> System.out.println(line));
-//        stream.close(); // 자원 누수를 방지하기 위한 close() 메소드
+        Stream<String> stream = Files.lines(path, Charset.defaultCharset());
+        // lines() 파일에서 모든 line을 읽어서 Stream으로 리턴
 
-        try (Stream<String> stream = Files.lines(path, Charset.defaultCharset())) {
-            stream.forEach(line -> System.out.println(line));
-        } catch (Exception e) {
-            e.printStackTrace();
-        } // 리소스 닫기를 통한 try-with-resources 문법(얘도 자원 누수 방지)
+        stream.forEach(System.out::println);
+        stream.close(); // 자원 누수를 방지하기 위한 close() 메소드
+
+//        try (Stream<String> stream = Files.lines(path, Charset.defaultCharset())) {
+//            stream.forEach(System.out::println);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } // 리소스 닫기를 통한 try-with-resources 문법(얘도 자원 누수 방지)
     }
 }
